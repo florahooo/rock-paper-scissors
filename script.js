f13410050682771577649397d7320d7ad7447a1c
Flora Hu <floratllh@gmail.com>
2023-08-23 06:38:25 -0400

    added conditional statements in playRound()

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
+    playerSelection.toLowerCase();
+
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Paper destroys rock, you win!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Scissors destroys paper, you win!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Rock destroys scissors, you win!";
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Paper destroys rock, you lose!";
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Scissors destroys paper, you lose!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Rock destroys scissors, you lose!";
+    }
+
 }
 
 function game(){
